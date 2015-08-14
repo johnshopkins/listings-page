7649307db7cbacf73d0354c5eff9291da9daa7ac
Jen Wachter <jenleighkelly@gmail.com>
2015-08-14 10:59:19 -0400

    Added required libraries and shims

--- app/views/NoResults.js
+++ app/views/NoResults.js
@@ -5,41 +5,41 @@ var Backbone  = require("../../shims/backbone");
 
 var templates = { noresults: require("../../templates/noresults.html") };
 
-// /**
-//  * Views.NoResults
-//  *
-//  * Controls the display of the no results message
-//  *
-//  */
-//
-// module.exports = Backbone.View.extend({
-//
-//   template: templates.noresults,
-//   className: "noresults column",
-//
-//   isShowing: function () {
-//
-//     return this.$el.hasClass("show");
-//
-//   },
-//
-//   show: function () {
-//
-//     this.$el.addClass("show");
-//
-//   },
-//
-//   hide: function () {
-//
-//     this.$el.removeClass("show");
-//
-//   },
-//
-//   render: function () {
-//
-//     this.$el.append(this.template());
-//     return this;
-//
-//   }
-//
-// });
+/**
+ * Views.NoResults
+ *
+ * Controls the display of the no results message
+ *
+ */
+
+module.exports = Backbone.View.extend({
+
+  template: templates.noresults,
+  className: "noresults column",
+
+  isShowing: function () {
+
+    return this.$el.hasClass("show");
+
+  },
+
+  show: function () {
+
+    this.$el.addClass("show");
+
+  },
+
+  hide: function () {
+
+    this.$el.removeClass("show");
+
+  },
+
+  render: function () {
+
+    this.$el.append(this.template());
+    return this;
+
+  }
+
+});
