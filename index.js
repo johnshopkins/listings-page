9e7e3ff49795a73af8d583ddb0a894c427e7786b
Jen Wachter <jenleighkelly@gmail.com>
2015-08-17 14:34:51 -0400

    Added Filter and FilterSet to list of views that are extendable

--- index.js
+++ index.js
@@ -10,6 +10,8 @@ module.exports = {
     filters: {
       CheckboxFilter: require("./app/views/filters/CheckboxFilter"),
       CheckboxFilterSet: require("./app/views/filters/CheckboxFilterSet"),
+      Filter: require("./app/views/filters/Filter"),
+      FilterSet: require("./app/views/filters/FilterSet"),
       SearchFilter: require("./app/views/filters/SearchFilter"),
       SearchFilterSet: require("./app/views/filters/SearchFilterSet")
     }
