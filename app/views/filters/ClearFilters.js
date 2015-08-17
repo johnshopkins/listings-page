e18937fdf2fb5656bb424ff105ac5b1f64baad5a
Jen Wachter <jenleighkelly@gmail.com>
2015-08-17 11:14:11 -0400

    Removed 'filter' class from clear filter button container

--- app/views/filters/ClearFilters.js
+++ app/views/filters/ClearFilters.js
@@ -13,7 +13,7 @@ var templates = { clear: require("../../../templates/filters/clear-button.html")
 
 module.exports = Backbone.View.extend({
 
-  className: "filter",
+  className: "clear-button",
   template: templates.clear,
 
   events: {
